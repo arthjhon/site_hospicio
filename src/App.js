2474c21b6623cc91da47098d3c265f41c770d902
Arthur Jhonathas <arthurjhonathaslima@outlook.com>
2025-01-14 11:13:34 -0300

    git

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Staff from './assets/pages/staff/staff';
 import Home from './assets/pages/home/home';
 import AdminPanel from './assets/pages/adminPages/AdminPanel';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importação correta
-import Container_times from './assets/pages/campeonatos/partials/container_times';
+import PoliticaDePrivacidade from './assets/pages/global/politica_de_privacidade';
+import TermoDeUso from './assets/pages/global/termos_de_uso';
+import Campeonatos from './assets/pages/campeonatos/campeonatos';
+import Parceiros from './assets/pages/parceiros/parceiros';
 
 function App() {
   return (
@@ -16,8 +19,11 @@ function App() {
         <Routes>
           <Route path="/" element={ <Home /> } />
           <Route path="/staff" element={<Staff />} />
-          <Route path="/campeonatos" element= {<Container_times/>} />
+          <Route path="/campeonatos" element= {<Campeonatos/>} />
           <Route path="/painel" element={< AdminPanel/>} />
+          <Route path="/politicas-de-privacidade" element={<PoliticaDePrivacidade/> }/>
+          <Route path="/termos-de-uso" element={<TermoDeUso />} />
+          <Route path="/parceiros" element={<Parceiros/>} />
         </Routes>
       </main>
       <Footer />
--- src/assets/pages/global/header.js
+++ src/assets/pages/global/header.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import './css/header.css'
-import LogoHospic from "../../src/logos/hospic_logos/logoHospic.png"
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './css/header.css';
+import LogoHospic from "../../src/logos/hospic_logos/logoHospic.png";
 
 function Header() {
+  const location = useLocation();  // Obtém a localização atual da URL
+
   return (
     <header className='header-site'>
         <div className="logo">
@@ -11,39 +14,47 @@ function Header() {
         </div>
         <nav className="navbar">
             <ul>
-            <li>
-                <a href="/" className="active">
-                <i className="fas fa-home" /> Home
-                </a>
-            </li>
-            <li>
-                <a href="/staff">
-                <i className="fas fa-user" /> Staff
-                </a>
-            </li>
-            <li>
-                <a href="/campeonatos">
-                <i className="fa-solid fa-trophy" /> Campeonatos
-                </a>
-            </li>
-            <li>
-                <a href="/partners">
-                <i className="fas fa-handshake" /> Parceiros
-                </a>
-            </li>
-            <li>
-                <a href="/portal-hstv">
-                <i className="fas fa-newspaper" /> HSTV
-                </a>
-            </li>
+                <li>
+                    <Link 
+                        to="/" 
+                        className={location.pathname === '/' ? 'active' : ''}>
+                        <i className="fas fa-home" /> Home
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to="/staff" 
+                        className={location.pathname === '/staff' ? 'active' : ''}>
+                        <i className="fas fa-user" /> Staff
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to="/campeonatos" 
+                        className={location.pathname === '/campeonatos' ? 'active' : ''}>
+                        <i className="fa-solid fa-trophy" /> Campeonatos
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to="/parceiros" 
+                        className={location.pathname === '/parceiros' ? 'active' : ''}>
+                        <i className="fas fa-handshake" /> Parceiros
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to="/portal-hstv" 
+                        className={location.pathname === '/portal-hstv' ? 'active' : ''}>
+                        <i className="fas fa-newspaper" /> HSTV
+                    </Link>
+                </li>
             </ul>
         </nav>
         <div>
-
         </div>
     </header>
-
-  )
+  );
 }
 
-export default Header
+export default Header;
